36f1266cabbbde51c045ec9de0cdef8f007e5c6c
praveenreddymudupu <mpraveenreddy2001@gmail.com>
2026-02-23 20:02:22 +0530

    Update gemini_client.py

--- backend/gemini_client.py
+++ backend/gemini_client.py
@@ -13,5 +13,5 @@ class GeminiClient:
         self.model = genai.GenerativeModel("gemini-2.5-flash")
 
     def generate(self, prompt):
-    response = self.model.generate_content(prompt)
-    return response.text
+        response = self.model.generate_content(prompt)
+        return response.text
