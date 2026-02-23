e181368c0af60aff838d637311331a6900d926f8
praveenreddymudupu <mpraveenreddy2001@gmail.com>
2026-02-23 20:11:08 +0530

    Update gemini_client.py

--- backend/gemini_client.py
+++ backend/gemini_client.py
@@ -3,7 +3,7 @@ import google.generativeai as genai
 
 class GeminiClient:
     def __init__(self):
-        api_key = os.getenv("GOOGLE_API_KEY")
+        api_key = os.getenv("GEMINI_API_KEY")
 
         if not api_key:
             raise ValueError("GEMINI_API_KEY not found.")
