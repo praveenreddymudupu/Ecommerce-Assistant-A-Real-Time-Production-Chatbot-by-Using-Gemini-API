02ec7a381228d236063ab95d490311e7f07af2bd
praveenreddymudupu <mpraveenreddy2001@gmail.com>
2026-02-23 20:00:29 +0530

    Update gemini_client.py

--- backend/gemini_client.py
+++ backend/gemini_client.py
@@ -1,26 +1,17 @@
 import os
 import google.generativeai as genai
-from dotenv import load_dotenv
-
-load_dotenv()
 
 class GeminiClient:
     def __init__(self):
-        api_key = os.getenv("GEMINI_API_KEY")
+        api_key = os.getenv("GOOGLE_API_KEY")
 
         if not api_key:
             raise ValueError("GEMINI_API_KEY not found.")
 
-        self.client = genai.Client(api_key=api_key)
+        genai.configure(api_key=api_key)
+
+        self.model = genai.GenerativeModel("gemini-2.5-flash")
 
     def generate(self, prompt):
-        try:
-            response = self.client.models.generate_content(
-                model="gemini-2.5-flash",
-                contents=prompt
-            )
-
-            return response.text
-
-        except Exception as e:
-            return f"⚠️ Technical Error: {str(e)}"
+    response = self.model.generate_content(prompt)
+    return response.text
